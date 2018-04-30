83d17fccb0d6c7409da20391d6fe250d2270369e
xuzengqiang <253948113@qq.com>
2018-04-30 22:08:39 +0800

    wapp
    新增注释

--- src/wapp.js
+++ src/wapp.js
@@ -1,3 +1,10 @@
+/**
+ * @copyright (c) 2018, www.wicoder.net. All rights reserved.
+ * @fileOverview wapp.js
+ * @author xuzengqiang <253948113@qq.com>
+ * @date 2018-04-30 10:05:42
+ * @version 1.0.0
+ */
 (function(global, factory) {
     "use strict";
 
