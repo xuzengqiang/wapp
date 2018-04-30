a69be862dc024f3b12115d6a63e19200b89269bc
xuzengqiang <253948113@qq.com>
2018-04-30 22:04:30 +0800

    Initial commit
    新增grunt和uglify插件

--- src/wapp.js
+++ src/wapp.js
@@ -1,10 +1,15 @@
-/**
- * @copyright (c) 2018, www.wicoder.net. All rights reserved.
- * @fileOverview wapp.js
- * @author xuzengqiang <253948113@qq.com>
- * @date 2018-04-30 06:55:15
- * @version 1.0.0
- */
-(function() {
-    console.error("wapp");
-})();
+(function(global, factory) {
+    "use strict";
+
+    factory(global);
+})(typeof window !== "undefined" ? window : this, function(window) {
+    var document = window.document;
+    var location = window.location;
+    var WAPP = {};
+
+    /**
+     * 对外暴露接口
+     * @since 1.0.0
+     */
+    window.WAPP = WAPP;
+});
