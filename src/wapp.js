c4da03046b3443143c8d0340d566b19f223dd16e
xuzengqiang <253948113@qq.com>
2018-05-29 01:22:35 +0800

    删除node_modules依赖

--- src/wapp.js
+++ src/wapp.js
@@ -36,12 +36,31 @@
     })()
 
     /**
+     * APP网络请求封装
+     * @since 1.0.0
+     */
+    WAPP.network = (function() {
+        var Network = {}
+
+        return Network
+    })()
+
+    /**
      * APP ajax请求封装
      * @since 1.0.0
      */
     WAPP.ajax = (function() {
         var Ajax = {}
         return Ajax
+    })()
+
+    /**
+     * APP文件操作
+     * @since 1.0.0
+     */
+    WAPP.file = (function() {
+        var File = {}
+        return File
     })()
 
     /**
